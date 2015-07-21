a1486ad025bd529eef1f423ba59da4cb0ee90a4f
Chris Lyon <chrisl@webdev.local>
2015-07-21 14:37:51 -0700

    initial commit

---
+++ codename.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import random
+
+
+def randWord(wordFile):
+	with open(wordFile) as f:
+		return random.choice(f.read().splitlines())
+
+
+def generateCodeName():
+	return randWord('data/adj.txt') + ' ' + randWord('data/verb.txt')
+
+
+if __name__ == '__main__':
+	print generateCodeName()
+
